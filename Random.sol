84a8c5d92114d36d5c22ad3cb82d0f1e02a3dc1f
Shubham <sjais789@users.noreply.github.com>
2019-09-12 14:34:08 +0200

    Update Random.sol

--- Random.sol
+++ Random.sol
@@ -3,12 +3,14 @@ pragma  solidity ^0.6.0;
 
 contract entropy{
     
+    // dynamic array and admin address 
     
     address [] public  participants;
     
     
-    
-    
+   constructor() public{
+        
+        admin = msg.sender;
     
     
     
